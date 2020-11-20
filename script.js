50da99ef3248d5616b83cfd4e741d12b3d55fda5
Visvaldas <visva.rapalis@gmail.com>
2020-11-20 08:06:09 +0200

    Select elements from the DOM

---
+++ script.js
@@ -0,0 +1,7 @@
+const container = document.querySelector('.container');
+const seats = document.querySelectorAll('.row .seat:not(.occupied)');
+const count = document.getElementById('count');
+const total = document.getElementById('total');
+const movieSelect = document.getElementById('movie');
+
+const ticketPrice = +movieSelect.nodeValue;
